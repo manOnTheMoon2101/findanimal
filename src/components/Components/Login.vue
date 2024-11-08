97d1ed756b9960651691214db56c5321f40c2745
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-11-08 23:05:58 +0200

    updates

--- src/components/Components/Login.vue
+++ src/components/Components/Login.vue
@@ -8,11 +8,10 @@ const imageSrc = dogs;
 
 <template>
     <div class="flex flex-row h-screen">
-        <div class="bg-green-900 w-4/6">
-            <img :src="imageSrc" alt="Dogs">
+        <div class="bg-green-900 w-4/6 rounded-tr-lg rounded-br-lg">
+            <img :src="imageSrc" alt="Dogs" class="rounded-tr-lg rounded-br-lg">
         </div>
-        <div class="w-2/6 flex items-center justify-center">
-
+        <div class="w-2/6 flex items-center justify-center rounded-lg">
             <div class="flex flex-col">
                 <div>
                     <h1 class="text-6xl">Welcome</h1>
