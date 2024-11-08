a96e1f6fad013fbcbd44b4e47df15201d51d701c
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-11-08 22:44:36 +0200

    dog added

--- src/components/Components/Login.vue
+++ src/components/Components/Login.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import Input from '../ui/input/Input.vue';
 import Button from '../ui/button/Button.vue';
+import dogs from "@/assets/dogs.jpg"
+
+const imageSrc = dogs;
 </script>
 
 <template>
     <div class="flex flex-row h-screen">
         <div class="bg-green-900 w-4/6">
-
+            <img :src="imageSrc" alt="Dogs">
         </div>
-        <div class="bg-blue-900 w-2/6 flex items-center justify-center">
+        <div class="w-2/6 flex items-center justify-center">
 
             <div class="flex flex-col">
+                <div>
+                    <h1 class="text-6xl">Welcome</h1>
+                </div>
                 <div class="grid w-full max-w-sm items-center gap-1.5">
                     <Label for="email">Email</Label>
                     <Input id="email" type="email" placeholder="Email" />
@@ -20,7 +26,7 @@ import Button from '../ui/button/Button.vue';
                     <Input id="password" type="password" placeholder="Password" />
                 </div>
 
-                <Button>Login</Button>
+                <Button class="bg-green-950">Login</Button>
 
                 <div>
                     <span>Log in as Guest</span>
