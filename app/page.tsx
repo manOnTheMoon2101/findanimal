79fe38a1f7ca99378c2945a9e97f40dfacdd7ade
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-11-17 17:49:01 +0200

    floating button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Header from "@/components/Header";
 import Body from "@/components/Body";
+import { MdInfo } from "react-icons/md";
 export default function Home() {
   return (
     <div>
       <Header />
       <Body/>
+
+      <button className="fixed bottom-5 right-5 bg-blue-500 text-white p-4 rounded-full shadow-lg text-2xl hover:bg-blue-600 focus:outline-none">
+      <MdInfo />
+      </button>
     </div>
   );
 }
