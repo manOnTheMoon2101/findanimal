5588d572889fff2018213c571301d5e1ad9d12e5
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-11-17 17:49:18 +0200

    format

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div>
       <Header />
       <Body/>
-
       <button className="fixed bottom-5 right-5 bg-blue-500 text-white p-4 rounded-full shadow-lg text-2xl hover:bg-blue-600 focus:outline-none">
       <MdInfo />
       </button>
