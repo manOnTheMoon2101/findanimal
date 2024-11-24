c7f2b1ed5f73bf138e991bc252d41d3aeccc0bb8
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-11-24 22:23:47 +0200

    footer added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Body from "@/components/Body";
 import { MdInfo } from "react-icons/md";
 import { InfoDialog } from "@/components/Body/info/Info";
+import Footer from "@/components/Footer/footer";
 export default function Home() {
   return (
     <div className="m-5">
@@ -14,6 +15,10 @@ export default function Home() {
       {/* <div className="fixed bottom-5 right-5 p-2 z-20">
         <InfoDialog />
       </div> */}
+
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
