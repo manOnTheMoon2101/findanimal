8c5d38253448bea28e65662eaad09a8073ef5403
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-11-27 19:34:22 +0200

    notes styling

--- components/Body/info/Info.tsx
+++ components/Body/info/Info.tsx
@@ -7,6 +7,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { MdInfo } from "react-icons/md";
 export function InfoDialog() {
   return (
     <Dialog>
@@ -15,17 +16,28 @@ export function InfoDialog() {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Note</DialogTitle>
+          <DialogTitle className="flex justify-center">
+            Note
+            <MdInfo className="text-accent" />
+          </DialogTitle>
         </DialogHeader>
-        <DialogDescription>
-          The project I’ve developed is an informational website designed for
+        <DialogDescription className="text-center">
+         <span>
+         The project I’ve developed is an informational website designed for
           our local animal adoption center. It serves as a platform to showcase
           the animals currently available for adoption, providing potential pet
           owners with up-to-date details about each animal's breed, age, and
-          temperament. By offering easy access to this information, the website
+          temperament. 
+         </span>
+          <br/>
+          <br/>
+          
+          <span>
+          By offering easy access to this information, the website
           aims to promote the adoption of pets in need and help connect them
           with loving homes. This project is a step forward in supporting our
           community’s efforts to rescue and rehome animals.
+          </span>
         </DialogDescription>
       </DialogContent>
     </Dialog>
