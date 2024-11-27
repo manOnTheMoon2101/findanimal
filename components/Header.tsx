de67c637c06e6c718db71b7118677bcf235f1d0e
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-11-27 20:00:10 +0200

    Header added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Menu } from "./Header/menu";
+import { FaDog } from "react-icons/fa";
+import { McLaren } from "next/font/google";
+const mclaren = McLaren({
+  weight: "400",
+  subsets: ["latin"],
+});
 function Header() {
   return (
-    <div className="flex justify-end">
+    <div className="flex justify-between items-center">
+      <h1 className={`flex text-4xl ${mclaren.className}`}>
+        Find
+        <span>
+          <FaDog className="text-accent" />
+        </span>
+        nimal
+      </h1>
       <Menu />
     </div>
   );
